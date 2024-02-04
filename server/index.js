3525ac028ce2e3f4e17104f4562222a2a99c3798
AFSAL333 <afsalafs8008@gmail.com>
2024-02-04 11:29:52 +0530

    cors updated

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(
   cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
-    methods: ["GET", "POST", "PUT"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
