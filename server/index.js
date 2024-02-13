37eda4152f1e4890ff0723305b3d08a6b9299bf4
AFSAL333 <afsalafs8008@gmail.com>
2024-02-13 20:50:59 +0530

    gives cors access

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: true,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
