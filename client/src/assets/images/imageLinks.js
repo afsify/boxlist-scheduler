46acd5d21b27c860723f6f447c03c03e916e87f9
AFSAL333 <afsalafs8008@gmail.com>
2024-12-02 15:43:32 +0530

    image link updated

--- client/src/assets/images/imageLinks.js
+++ client/src/assets/images/imageLinks.js
@@ -1,6 +1,6 @@
 const imageLinks = {
-  logo: "https://res.cloudinary.com/cloudverse/image/upload/v1721893242/CODSIFY/codsify.png",
-  profile: 'https://res.cloudinary.com/cloudverse/image/upload/v1702630432/CODEVERSE/profile.png',
+  logo: "https://res.cloudinary.com/afsify/image/upload/v1733133377/codisify/codisify.png",
+  profile: 'https://res.cloudinary.com/afsify/image/upload/v1702630432/codisify/profile.png',
 };
 
 export default imageLinks;
